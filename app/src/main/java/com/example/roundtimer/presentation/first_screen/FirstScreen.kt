74aee710734128d0157fd0aa4e5bf98baf2f0bcf
netbug94 <netbug94@gmail.com>
2024-10-05 20:11:00 -0700

    Added imePadding

--- app/src/main/java/com/example/roundtimer/presentation/first_screen/FirstScreen.kt
+++ app/src/main/java/com/example/roundtimer/presentation/first_screen/FirstScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.HorizontalDivider
@@ -64,7 +65,8 @@ fun FirstScreenContent(
 
     Column(
         modifier = Modifier.fillMaxSize()
-            .systemBarsPadding(),
+            .systemBarsPadding()
+            .imePadding(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
