ddac926f84ec07feaebfbae0d5d53f67b5234526
netbug94 <netbug94@gmail.com>
2024-10-06 19:07:51 -0700

    dropdown menu with less code and better design

--- app/src/main/java/com/example/roundtimer/presentation/first_screen/settings/SettingsButton.kt
+++ app/src/main/java/com/example/roundtimer/presentation/first_screen/settings/SettingsButton.kt
@@ -1,34 +1,112 @@
 package com.example.roundtimer.presentation.first_screen.settings
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.roundtimer.R
+import com.example.roundtimer.ui.theme.customColorScheme
 
 @Composable
 fun SettingsButton() {
+    var expanded by remember { mutableStateOf(false) }
+
     val isDarkTheme = isSystemInDarkTheme()
     val imageId = if (isDarkTheme) R.drawable.lightdots else R.drawable.darkdots
+
+    val customColor = customColorScheme()
+    val textDropdownColor = customColorScheme().customTextColor
 
     Box(modifier = Modifier.size(25.dp)) {
         Image(
             painter = painterResource(id = imageId),
             contentDescription = if (isDarkTheme) "Dark theme dots" else "Light theme dots",
             contentScale = ContentScale.Fit,
-            modifier = Modifier.fillMaxSize().padding(horizontal = 4.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 4.dp)
                 .clip(RoundedCornerShape(2.5.dp))
-                .clickable { /*TODO*/ }
+                .clickable { expanded = true }
         )
+
+        Column(modifier = Modifier.fillMaxWidth().padding(end = 8.dp)) {
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.width(84.dp)
+                    .background(customColor.customRippleColor)
+            ) {
+                DropdownMenuItem(modifier = Modifier.fillMaxWidth(),
+                    text = {
+                        Text(
+                            text = "Settings",
+                            color = textDropdownColor,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center) },
+                    onClick = {
+                        // Handle option 2 click
+                        expanded = false
+                    }
+                )
+
+                HorizontalDivider(Modifier.height(2.dp), color = customColor.customTextColor)
+
+                DropdownMenuItem(
+                    text = {
+                        Text(
+                            text = "Tricks",
+                            color = textDropdownColor,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center
+                        )
+                    },
+                    onClick = {
+                        // Handle option 2 click
+                        expanded = false
+                    }
+                )
+
+                HorizontalDivider(Modifier.height(2.dp), color = customColor.customTextColor)
+
+                DropdownMenuItem(
+                    text = {
+                        Text(
+                            text = "About",
+                            color = textDropdownColor,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center
+                        )
+                    },
+                    onClick = {
+                        // Handle option 2 click
+                        expanded = false
+                    }
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/roundtimer/presentation/first_screen/settings/SettingsButtonPreview.kt
+++ app/src/main/java/com/example/roundtimer/presentation/first_screen/settings/SettingsButtonPreview.kt
@@ -1,15 +1,21 @@
 package com.example.roundtimer.presentation.first_screen.settings
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,9 +26,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.roundtimer.R
-
+import com.example.roundtimer.ui.theme.customColorScheme
 
 @Composable
 fun SettingsButtonPreview() {
@@ -30,6 +37,11 @@ fun SettingsButtonPreview() {
 
     val isDarkTheme = isSystemInDarkTheme()
     val imageId = if (isDarkTheme) R.drawable.lightdots else R.drawable.darkdots
+
+    val customColor = customColorScheme()
+    val textDropdownColor = customColor.customTextColor
+
+    val menuItems = listOf("Settings", "Tricks", "About")
 
     Box(modifier = Modifier.size(25.dp)) {
         Image(
@@ -40,37 +52,38 @@ fun SettingsButtonPreview() {
                 .fillMaxSize()
                 .padding(horizontal = 4.dp)
                 .clip(RoundedCornerShape(2.5.dp))
-                .clickable { expanded = true } // Show dropdown menu when clicked
+                .clickable { expanded = true }
         )
 
-        // Dropdown Menu
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-            modifier = Modifier
-                .size(150.dp) // Set size if needed
-        ) {
-            DropdownMenuItem(
-                text = { Text(text = "Settings") },
-                onClick = {
-                    // Handle option 1 click
-                    expanded = false
+        Column(modifier = Modifier.fillMaxWidth().padding(end = 8.dp)) {
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.width(84.dp).background(customColor.customRippleColor)
+            ) {
+                menuItems.forEachIndexed { index, item ->
+                    DropdownMenuItem(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = {
+                            Text(
+                                text = item,
+                                color = textDropdownColor,
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center
+                            )
+                        },
+                        onClick = {
+
+                            expanded = false
+                        }
+                    )
+
+                    if (index < menuItems.lastIndex) {
+                        HorizontalDivider(Modifier.height(2.dp).padding(horizontal = 8.dp), color = customColor.customTextColor)
+                    }
                 }
-            )
-            DropdownMenuItem(
-                text = { Text(text = "About") },
-                onClick = {
-                    // Handle option 2 click
-                    expanded = false
-                }
-            )
-            DropdownMenuItem(
-                text = { Text(text = "Tips") },
-                onClick = {
-                    // Handle option 3 click
-                    expanded = false
-                }
-            )
+            }
         }
     }
 }
+
