2b4e0d87d442fe346400305fbc9e39d871a95348
netbug94 <bongohmo@gmail.com>
2024-10-09 09:10:28 -0700

    Added cool wallpaper to five second screen

--- app/src/main/java/com/example/roundtimer/presentation/round_screen/RoundScreen.kt
+++ app/src/main/java/com/example/roundtimer/presentation/round_screen/RoundScreen.kt
@@ -1,8 +1,6 @@
 package com.example.roundtimer.presentation.round_screen
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,8 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
