c320f2e81e52b8329b8fd6c62680f76ae191b8e5
netbug94 <bongohmo@gmail.com>
2024-10-30 23:16:52 -0600

    better ui for and snackbar added, also xml strings for both english and spanish

--- app/src/main/java/com/example/roundtimer/presentation/saved_workout_screen/SavedWorkoutScreen.kt
+++ app/src/main/java/com/example/roundtimer/presentation/saved_workout_screen/SavedWorkoutScreen.kt
@@ -24,6 +24,10 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -31,16 +35,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.roundtimer.R
 import com.example.roundtimer.domain.room_domain.WorkoutRoomEntity
+import kotlinx.coroutines.launch
 
 @Composable
 fun SavedWorkoutScreen(
@@ -60,40 +68,65 @@ fun WorkoutListScreen(
 ) {
     val workouts by roomViewModel.allWorkouts.observeAsState(emptyList())
     val workoutToDelete = roomViewModel.workoutToDelete.observeAsState().value
+    @Suppress("SpellCheckingInspection")
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
-    LazyColumn(
-        modifier = Modifier
-            .systemBarsPadding()
-            .padding(horizontal = 8.dp)
-    ) {
-        items(
-            items = workouts,
-            key = { workout -> workout.id } // Ensure 'id' is unique
-        ) { workout ->
-            WorkoutListItem(
-                workout = workout,
-                onDelete = {
-                    roomViewModel.showDeleteConfirmation(workout)
+    val deleteSuccessful = stringResource(R.string.DeleteSuccessful)
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(paddingValues)
+                .systemBarsPadding()
+                .padding(horizontal = 8.dp)
+        ) {
+            items(
+                items = workouts,
+                key = { workout -> workout.id }
+            ) { workout ->
+                WorkoutListItem(
+                    workout = workout,
+                    onDelete = {
+                        roomViewModel.showDeleteConfirmation(workout)
+                    },
+                    onEdit = { updatedWorkout ->
+                        roomViewModel.updateRoomWorkoutName(updatedWorkout)
+                    },
+                    onClick = { onWorkoutSelected(workout) },
+                    showSnackbar = { message ->
+                        coroutineScope.launch {
+                            snackbarHostState.showSnackbar(
+                                message,
+                                duration = SnackbarDuration.Short
+                            )
+                        }
+                    }
+                )
+            }
+        }
+
+        if (workoutToDelete != null) {
+            DeleteConfirmationDialog(
+                workout = workoutToDelete,
+                onConfirm = {
+                    roomViewModel.deleteRoomWorkout(workoutToDelete)
+                    roomViewModel.clearDeleteConfirmation()
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(
+                            deleteSuccessful,
+                            duration = SnackbarDuration.Short,
+
+                        )
+                    }
                 },
-                onEdit = { updatedWorkout ->
-                    roomViewModel.updateRoomWorkoutName(updatedWorkout)
-                },
-                onClick = { onWorkoutSelected(workout) }
+                onDismiss = {
+                    roomViewModel.clearDeleteConfirmation()
+                }
             )
         }
-    }
-
-    if (workoutToDelete != null) {
-        DeleteConfirmationDialog(
-            workout = workoutToDelete,
-            onConfirm = {
-                roomViewModel.deleteRoomWorkout(workoutToDelete)
-                roomViewModel.clearDeleteConfirmation()
-            },
-            onDismiss = {
-                roomViewModel.clearDeleteConfirmation()
-            }
-        )
     }
 }
 
@@ -102,10 +135,18 @@ fun WorkoutListItem(
     workout: WorkoutRoomEntity,
     onDelete: () -> Unit,
     onEdit: (WorkoutRoomEntity) -> Unit,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    @Suppress("SpellCheckingInspection")
+    showSnackbar: (String) -> Unit
 ) {
     var isEditing by rememberSaveable { mutableStateOf(false) }
     var editedName by rememberSaveable { mutableStateOf(workout.name) }
+    val textFieldTitle = stringResource(R.string.TextFieldTitleWorkoutListItem)
+    val updateSuccessful = stringResource(R.string.UpdatedSuccessful)
+    val cannotBeEmpty = stringResource(R.string.CannotBeEmpty)
+    val roundsString = stringResource(R.string.Rounds)
+    val roundTimeString = stringResource(R.string.RoundTimeString)
+    val restTimeString = stringResource(R.string.RestTimeString)
 
     Card(
         modifier = Modifier
@@ -116,7 +157,6 @@ fun WorkoutListItem(
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
 
-            // Title and Edit/Save Icon in a Row
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.fillMaxWidth()
@@ -125,15 +165,14 @@ fun WorkoutListItem(
                     TextField(
                         value = editedName,
                         onValueChange = { editedName = it },
-                        label = { Text("Workout Name") },
+                        label = { Text(textFieldTitle) },
                         singleLine = true,
                         keyboardOptions = KeyboardOptions.Default.copy(
                             capitalization = KeyboardCapitalization.Sentences
                         ),
                         textStyle = MaterialTheme.typography.titleLarge.copy(
                             fontWeight = FontWeight.SemiBold,
-                            fontStyle = FontStyle.Italic,
-                            fontSize = 18.sp // Adjust as desired
+                            fontStyle = FontStyle.Italic
                         ),
                         modifier = Modifier.weight(1f)
                     )
@@ -144,8 +183,10 @@ fun WorkoutListItem(
                                 val updatedWorkout = workout.copy(name = editedName)
                                 onEdit(updatedWorkout)
                                 isEditing = false
+                                showSnackbar(updateSuccessful)
+                            } else {
+                                showSnackbar(cannotBeEmpty)
                             }
-                            // Optionally, handle empty input (e.g., show a Snackbar)
                         },
                         modifier = Modifier.size(24.dp)
                     ) {
@@ -178,30 +219,31 @@ fun WorkoutListItem(
 
             Spacer(modifier = Modifier.height(6.dp))
 
-            // Workout Details
             Text(
-                text = "Rounds: ${workout.roundNumber}",
+                text = "$roundsString: ${workout.roundNumber}",
                 style = MaterialTheme.typography.bodyLarge
             )
             Text(
-                text = "Round Time: ${workout.roundMinutes}m ${workout.roundSeconds}s",
+                text = "$roundTimeString: ${workout.roundMinutes}m ${workout.roundSeconds}s",
                 style = MaterialTheme.typography.bodyLarge
             )
             Text(
-                text = "Rest Time: ${workout.restMinutes}m ${workout.restSeconds}s",
+                text = "$restTimeString: ${workout.restMinutes}m ${workout.restSeconds}s",
                 style = MaterialTheme.typography.bodyLarge
             )
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            // Delete and Cancel Buttons (Visible Only in Edit Mode)
+
             if (isEditing) {
                 Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Button(
-                        onClick = onDelete,
+                        onClick = {
+                            onDelete()
+                        },
                         colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error)
                     ) {
                         Text(
